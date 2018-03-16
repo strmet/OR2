ab6ebe05e9c4aa4ebe44aa676332cdcfe2a44a3c
Matteo Stringher <matteo.stringher@studenti.unipd.it>
2018-03-16 17:31:08 +0100

    Variables implementation added

--- src/main.py
+++ src/main.py
@@ -1,10 +1,57 @@
-import cplex
+from docplex.mp.model import Model
+from collections import namedtuple
+
+Edge = namedtuple("Edge", ["source", "destination"])
 
 
 def main():
+
     substations, turbines = read_turbines_file("wf01/wf01.turb")
     cables = read_cables_file("wf01/wf01_cb01.cbl")
-    print(cables)
+
+    n = len(turbines)  + len(substations)
+    num_cables = len(cables)
+
+    edges = [Edge(i, j) for i in range(1, n + 1) for j in range(1, n + 1)]
+
+    print(edges)
+
+    model = Model(agent='local', name='wind_farm')
+    #model.edges = edges
+    #print(model.edges)
+
+    for e in edges:
+        model.binary_var(name="y_{0}.{1}".format(e.source, e.destination))
+        model.binary_var(name="f_{0}.{1}".format(e.source, e.destination))
+
+    for e in edges:
+        for k in range(1, num_cables + 1):
+            model.binary_var(name="x_{0}.{1}.{2}".format(e.source, e.destination, k))
+            #print(model.get_var_by_name(name="x_{0}.{1}.{2}".format(e.source,e.destination, k)))
+
+    #for i in range(1, n + 1):
+    #    var = model.get_var_by_name("y_{0}.{1}".format(i, i))
+    #    model.add_equivalence(binary_var=var, linear_ct="eq", true_value=0)
+    #print(model.number_of_constraints)
+    '''
+    for i in range(1, n + 1):
+        for j in range(1, n + 1):
+            model.binary_var(name="y_{0}.{1}".format(i, j))
+            #print(model.get_var_by_name(name="y_{0}.{1}".format(i, j)))
+
+    for i in range(1, n + 1):
+        for j in range(1, n + 1):
+            model.binary_var(name="f_{0}.{1}".format(i, j))
+            #print(model.get_var_by_name(name="f_{0}.{1}".format(i, j)))
+
+    for i in range(1, n + 1):
+        for j in range(1, n + 1):
+            for k in range(1, num_cables + 1):
+                model.binary_var(name="x_{0}.{1}.{2}".format(i, j, k))
+                print(model.get_var_by_name(name="x_{0}.{1}.{2}".format(i, j, k)))
+    '''
+
+    #for i in range(1, n + 1):
 
 def read_turbines_file(name):
     file = open("../data/" + name, "r")
