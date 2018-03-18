ef91071de1cf3874d95272b1e41e7cdd0b296bb3
Matteo Stringher <matteo.stringher@studenti.unipd.it>
2018-03-18 20:13:20 +0100

    Added some checks, asked by professor, and instance class. Refactoring

--- src/__init__.py
+++ src/__init__.py
@@ -1,4 +1,4 @@
-from docplex.mp.model import Model
+from docplex.mp.model import Model, ModelStatistics
 from collections import namedtuple
 import math
 import plotly.offline as py
@@ -13,30 +13,50 @@ Cable = namedtuple("Cable", ["capacity", "price", "max_usage"])
 
 def main():
 
-    points = read_turbines_file("wf05/wf05.turb")
-    cables = read_cables_file("wf05/wf05_cb04.cbl")
+    inst = instance()
 
-    n = len(points)
-    num_cables = len(cables)
+    inst.turb_file = 'wf01/wf01.turb'
+    inst.cbl_file = 'wf01/wf01_cb01.cbl'
+    inst.name = 'Wind Farm wf01'
 
-    edges = [Edge(i, j) for i in range(0, n) for j in range(0, n)]
+    points, inst = read_turbines_file(inst)
+    cables, inst = read_cables_file(inst)
 
-    model = Model(name='wind_farm')
+    n = inst.n_nodes
+    num_cables = inst.num_cables
 
-    for e in edges:
-        model.binary_var(name="y_{0}.{1}".format(e.source + 1, e.destination + 1))
-        model.continuous_var(name="f_{0}.{1}".format(e.source + 1, e.destination + 1))
+    edges = [Edge(i, j) for i in range(inst.n_nodes) for j in range(inst.n_nodes)]
 
-    for e in edges:
-        for k in range(0, num_cables):
-            model.binary_var(name="x_{0}.{1}.{2}".format(e.source + 1, e.destination + 1, k + 1))
+    model = Model(name=inst.name)
+    model.set_time_limit(inst.time_limit)
+
+    # Add y_i.i variables
+    inst.y_start = model.get_statistics().number_of_variables
+    for index, edge in enumerate(edges):
+        model.binary_var(name="y_{0}.{1}".format(edge.source + 1, edge.destination + 1))
+        if ypos(index, inst) != model.get_statistics().number_of_variables - 1:
+            raise NameError('Number of variables and index do not match')
+
+    inst.f_start = model.get_statistics().number_of_variables
+    for index, edge in enumerate(edges):
+        model.continuous_var(name="f_{0}.{1}".format(edge.source + 1, edge.destination + 1))
+        if fpos(index, inst) != model.get_statistics().number_of_variables - 1:
+            raise NameError('Number of variables and index do not match')
+
+    inst.x_start = model.get_statistics().number_of_variables
+    for index, edge in enumerate(edges):
+        for k in range(inst.num_cables):
+            model.binary_var(name="x_{0}.{1}.{2}".format(edge.source + 1, edge.destination + 1, k + 1))
+
+        if xpos(index, k, inst) != model.get_statistics().number_of_variables - 1:
+            raise NameError('Number of variables and index do not match')
 
 
-    for i in range(n):
+    for i in range(inst.n_nodes):
         var = model.get_var_by_name("y_{0}.{1}".format(i + 1, i + 1))
         model.add_constraint(var == 0, ctname="y_{0}.{1}=0".format(i + 1, i + 1))
 
-    for i in range(n):
+    for i in range(inst.n_nodes):
         var = model.get_var_by_name("f_{0}.{1}".format(i + 1, i + 1))
         model.add_constraint(var == 0, ctname="y_{0}.{1}=0".format(i + 1, i + 1))
 
@@ -67,7 +87,7 @@ def main():
         model.add_constraint(
             model.get_var_by_name("y_{0}.{1}".format(edge.source + 1, edge.destination + 1))
             ==
-            model.sum(model.get_var_by_name("x_{0}.{1}.{2}".format(edge.source +1, edge.destination + 1, k + 1)) for k in range(num_cables))
+            model.sum(model.get_var_by_name("x_{0}.{1}.{2}".format(edge.source + 1, edge.destination + 1, k + 1)) for k in range(num_cables))
         )
 
     for edge in edges:
@@ -95,29 +115,32 @@ def main():
         )
     )
 
-    print("Solving...")
-    model.solve()
+    model.export_as_lp("mode.lp")
+    #print("Solving...")
+    #model.solve()
 
     #model.print_solution()
-    plot_solution(points)
+    #plot_solution(points, inst)
 
-def read_turbines_file(name):
-    file = open("../data/" + name, "r")
+def read_turbines_file(inst):
+    file = open("../data/" + inst.turb_file, "r")
     points = []
 
     for index, line in enumerate(file):
-        if index > 20: break
+        #if index > 20: break
         words = list(map(int, line.split()))
         points.append(
             Point(words[0], words[1], words[2])
         )
 
     file.close()
-    return points
+    inst.n_nodes = len(points)
+    return points, inst
 
 
-def read_cables_file(name):
-    file = open("../data/" + name, "r")
+def read_cables_file(inst):
+    file = open("../data/" + inst.cbl_file, "r")
+
     cables = []
     for index, line in enumerate(file):
         if index > 3: break
@@ -127,7 +150,8 @@ def read_cables_file(name):
         )
 
     file.close()
-    return cables
+    inst.num_cables = len(cables)
+    return cables, inst
 
 def get_distance(point1, point2):
     return math.sqrt(
@@ -136,7 +160,7 @@ def get_distance(point1, point2):
         (point1.y - point2.y)**2
     )
 
-def plot_solution(nodes):
+def plot_solution(nodes, inst):
     G = nx.Graph()
 
     for index, node in enumerate(nodes):
@@ -184,7 +208,7 @@ def plot_solution(nodes):
             colorscale='Greens',
             reversescale=True,
             color=[],
-            size=20,
+            size=10,
             colorbar=dict(
                 thickness=15,
                 title='Node Connections',
@@ -206,22 +230,52 @@ def plot_solution(nodes):
 
     fig = Figure(data=Data([edge_trace, node_trace]),
              layout=Layout(
-                title='<br>Wind Farm',
-                titlefont = dict(size=16),
-                showlegend = False,
-                hovermode = 'closest',
-                margin = dict(b=20,l=5,r=5,t=40),
-                annotations=[ dict(
+                title='<br>'+inst.name,
+                titlefont=dict(size=16),
+                showlegend=False,
+                hovermode='closest',
+                margin=dict(b=20,l=5,r=5,t=40),
+                annotations=[dict(
                     showarrow = False,
                     xref="paper", yref="paper",
                     x=0.005, y=-0.002 ) ],
                 xaxis=XAxis(showgrid=False, zeroline=False, showticklabels=False),
-                yaxis=YAxis(showgrid=False, zeroline=False, showticklabels=False)))
+                yaxis=YAxis(showgrid=False, zeroline=False, showticklabels=False))
+             )
 
     py.plot(fig, filename='networkx.html')
 
-def ypos(i, j):
-    return
+def ypos(offset, inst):
+    return inst.y_start + offset
+
+def fpos(offset, inst):
+    return inst.f_start + offset
+
+def xpos(offset, k, inst):
+    return inst.x_start + offset * inst.num_cables + k
+
+class instance():
+
+    ## Model
+    name = ''
+    y_start = 0
+    f_start = 0
+    x_start = 0
+
+    ## Input data
+    cbl_file = ''
+    turb_file = ''
+    n_nodes = 0
+    num_cables = 0
+
+    ## Parameters
+    #model_type
+    #num_threads
+    time_limit = 3600
+    # available_memory
+    # max_nodes
+    # cutoff
+    # integer_costs
 
 if __name__ == "__main__":
     main()
