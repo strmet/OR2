b051f35cbb9d7aaa44a0050a474b64559df2dec4
lucavenir <luca.venir@studenti.unipd.it>
2018-03-29 12:28:57 +0200

    Comb algorithm (most probably) finished

--- src/lib/inout.py
+++ src/lib/inout.py
@@ -1,11 +1,11 @@
 import networkx as nx
 import numpy as np
 
+"""
+    OLD/TESTING FUNCTIONS
+"""
 
-def test():
-    return "LOL"
-
-
+"""
 def g_input_read():
     # generiamo un grafo completo un po' a caso
     G = nx.complete_graph(100)
@@ -24,3 +24,47 @@ def p_input_read():
         p[id] = set(patient_genes)
 
     return p
+"""
+
+
+def read_patients(filename, genes_map):
+    file  = open(filename, "r")
+    patients={}
+    lines=file.readlines()
+    for l in lines:
+        s=l.split("\t")
+        # Nota: se un gene risulta mutato in un paziente ma non compare nell'elenco dei geni dato in input,
+        #       lo eliminiamo nell'elenco dei geni mutati di tale paziente, considerandolo (evidentemente)
+        #       non rilevante.
+        patients[s[0]]=set([genes_map[gene] for gene in s[1:] if gene in genes_map])
+    #print(patients)
+    return patients
+
+
+def read_genes(filename):
+    file  = open(filename, "r")
+    id_to_str={} # dato l'id del gene, d[id] = "stringa del gene"
+    str_to_id={} # data la stringa del gene, d[stringa] = "id del gene"
+    lines=file.readlines()
+    for l in lines:
+        s=l.split(" ")
+        gene_id = int(s[0])
+        gene_str = s[1].split("\t")[0]
+        id_to_str[gene_id] = gene_str
+        str_to_id[gene_str] = gene_id
+    #print("id_to_str: ", id_to_str)
+    #print("str_do_id: ", str_to_id)
+    #input()
+    #print (genes)
+    return id_to_str, str_to_id
+
+
+def loadNetwork(filename):
+    file  = open(filename, "r")
+    G=nx.Graph()
+    lines=file.readlines()
+    for l in lines:
+        s=l.split(" ")
+        s=[ int(ss) for ss in s]
+        G.add_edge(s[0], s[1] ,weight=s[2])
+    return G
